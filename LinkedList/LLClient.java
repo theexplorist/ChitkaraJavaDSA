e21f168fe6a312d4521f4a1d44e25c9c74c13df6
theexplorist <kartiksapra005@gmail.com>
2021-11-08 20:43:18 +0530

    8 Nov 21 - Linked List - 2

--- LinkedList/LLClient.java
+++ LinkedList/LLClient.java
@@ -4,19 +4,24 @@ public class LLClient {
 
 	public static void main(String[] args) throws Exception {
 		LinkedList ll = new LinkedList();
-		ll.addAtLast(50);
+		
 		//ll.getFirst();
 		//System.out.println(ll.head);
 		ll.addAtFirst(10);
 		ll.addAtFirst(20);
 		ll.addAtFirst(30);
 		ll.addAtLast(60);
+		//ll.addAtLast(70);
 		ll.display();
-		System.out.println(ll.getFirst());
-		System.out.println(ll.getLast());
-		System.out.println(ll.size());
-		
-		System.out.println(ll.getAtIndex(1));
+//		System.out.println(ll.getFirst());
+//		System.out.println(ll.getLast());
+//		System.out.println(ll.size());
+//		
+//		System.out.println(ll.getAtIndex(1));
+//		System.out.println(ll.deleteAtIndex(0));
+//		System.out.println(ll.deleteAtIndex(ll.size() - 1));
+		System.out.println(ll.midPointVal());
+		ll.display();
 	}
 
 }
--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -138,4 +138,98 @@ public class LinkedList {
 		return i;// node ka address
 	}
 
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+
+		return head == null;
+	}
+
+	public int deleteFirst() throws Exception {
+		// TODO Auto-generated method stub
+
+		if (this.isEmpty()) {
+			throw new Exception("Khaali hai LinkedList!");
+		}
+		Node i = head;
+		head = head.next;
+		i.next = null;
+		return i.val;
+	}
+
+	public int deleteLast() throws Exception {
+		// TODO Auto-generated method stub
+
+		if (size() == 1) {
+			return deleteFirst();
+		} else {
+			Node last_1 = getNodeAtIndex(this.size() - 2);
+			Node last = last_1.next;
+			int temp = last.val;
+			last_1.next = null;
+			return temp;
+		}
+	}
+
+	public int deleteAtIndex(int idx) throws Exception {
+		// TODO Auto-generated method stub
+
+		if (this.isEmpty()) {
+			throw new Exception("Khaali hai LinkedList!");
+		}
+		
+		if(idx >= size()) {
+			throw new Exception("Galat Index!");
+		}
+		if (idx == 0) {
+			return deleteFirst();
+		} else {
+			Node idx_1 = getNodeAtIndex(idx - 1);
+			Node idxN = idx_1.next;
+			Node idxPlus1 = idx_1.next.next;
+			idxN.next = null;
+			idx_1.next = idxPlus1;
+			return idxN.val;
+		}
+	}
+	
+	public void reverseActual() throws Exception {
+		// TODO Auto-generated method stub
+
+		if(this.isEmpty()) {
+			throw new Exception("Empty hai!");
+		}
+		Node prev = null;
+		Node curr = head;
+		
+		while(curr != null) {
+			Node currNext = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = currNext;
+		}
+		
+		head = prev;
+	}
+	
+	public int midPointVal() {
+		// TODO Auto-generated method stub
+
+		return midPointNode().val;
+	}
+	
+	public Node midPointNode() {
+		// TODO Auto-generated method stub
+
+		Node s = head;
+		Node f = head;
+		
+		while(f != null && f.next != null) {
+			
+			s = s.next;
+			f = f.next.next;//f -> last node, f.next = null, null.next = npe
+		}
+		
+		return s;
+	}
+
 }
